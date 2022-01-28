b4dd141c2b814e5953536fcf4dca2b6f35f69324
Andrew Dutson <Dutsandrew@gmail.com>
2022-01-28 13:14:17 -0700

    querySelectors added for DOM node access, fixed attribute names in HTML and CSS

---
+++ app.js
@@ -0,0 +1,23 @@
+// querySelectors to access DOM elements in JS
+const display = document.querySelector('.display');
+const arithmetic = document.querySelector('#arithmetic');
+const clear = document.querySelector('#clear');
+const backspace = document.querySelector('#backspace');
+const digitBtn = document.querySelectorAll('.digitBtn');
+const operatorBtn = document.querySelectorAll('.operatorBtn');
+
+const nine = document.querySelector('#9');
+const eight = document.querySelector('#8');
+const seven = document.querySelector('#7');
+const six = document.querySelector('#6');
+const five = document.querySelector('#5');
+const four = document.querySelector('#4');
+const three = document.querySelector('#3');
+const two = document.querySelector('#2');
+const one = document.querySelector('#1');
+
+const multiply = document.querySelector('#multiply');
+const divide = document.querySelector('#divide');
+const add = document.querySelector('#add');
+const subract = document.querySelector('#subtract');
+const sum = document.querySelector('#sum');
