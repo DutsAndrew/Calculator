35995c1accb1986fbb2a4d829d6cafbb4679bdfd
Andrew Dutson <Dutsandrew@gmail.com>
2022-02-02 10:08:14 -0700

    added getNumber and getOperator to store and send number1, number2, and operator to operate()

--- app.js
+++ app.js
@@ -25,6 +25,74 @@ const subract = document.querySelector('#subtract');
 // set number1 and number2 as mutatable variables
 let number1 = '';
 let number2 = '';
+let selectedOperator = '';
+
+// Event Listeners
+clear.addEventListener('click', clearIt);
+backspace.addEventListener('click', backSpaceIt);
+equalsBtn.addEventListener('click', operate);
+operatorBtn.forEach((button) => button.addEventListener('click', () => getOperator(button.textContent)));
+digitBtn.forEach((button) => button.addEventListener('click', () => getNumber(button.textContent)));
+
+// Calculator Functionality
+function clearIt() {
+    console.log("clear button is working");
+    display.textContent = '';
+    number1 = '';
+    number2 = '';
+    selectedOperator = '';
+};
+
+function backSpaceIt() {
+    console.log("backspace button is working");
+    display.textContent = display.textContent.toString().slice(0, -1);
+};
+
+function getNumber(number) {
+    if (number1 === '') {
+        number1 = number;
+        display.textContent += number1;
+        console.log(`number1: ${number1} has been saved`);
+    } else if (number1 !== 'null') {
+        number2 = number;
+        display.textContent += number2;
+        console.log(`number2: ${number2} has been saved`);
+    } else if (number1 !== null && number2 !== null) {
+        operate(number1, number2);
+        console.log("sending number1 and number2 to operate");
+    }
+};
+
+function getOperator(operator) {
+    if (selectedOperator === '') {
+        console.log("operatorBtn was clicked");
+        selectedOperator = operator;
+        display.textContent += (" " + `${selectedOperator}` + " ");
+        console.log(selectedOperator);
+    } else if (selectedOperator !== 'null') {
+        operate(selectedOperator);
+    } else {
+        clearIt();
+        console.log("getOperator did not save or send an operator to operate");
+    }
+};
+
+function operate(operator, number1, number2) {
+    console.log(`operator selected was ${operator}, number1 is ${number1}, and number2 is ${number2}`);
+
+    switch(operator) {
+        case 'x':
+            return multiplyIt(number1, number2);
+        case '/':
+            return divideIt(number1, number2);
+        case '+':
+            return addIt(number1, number2);
+        case '-':
+            return subtractIt(number1, number2);
+        default:
+            return null;
+    };
+}
 
 //math logic
 function addIt(number1, number2) {
@@ -60,25 +128,25 @@ function divideIt(number1, number2) {
     return sum;
 };
 
-function powerIt(power1, power2) {
+function powerIt(number1, number2) {
     let sum;
-    sum = power1 * power2;
+    sum = number1 * number2;
     return sum;
 };
 
-function factorialIt(factorial) {
+function factorialIt(number1) {
     let sum;
-    sum = factorial;
+    sum = number1;
 
-    if (factorial < 0) {
+    if (number1 < 0) {
         return (-1);
-    } else if (factorial === 0 || factorial === 1) {
+    } else if (number1 === 0 || number1 === 1) {
         return (1);
     }
 
-    while (factorial > 1) {
-        factorial--;
-        sum *= factorial;
+    while (number1 > 1) {
+        number1--;
+        sum *= number1;
     }
     return sum;
 };
@@ -86,47 +154,3 @@ function factorialIt(factorial) {
 function roundIt(number) {
     return Math.round(number * 1000) / 1000;
 }
-
-// Event Listeners
-clear.addEventListener('click', clearIt);
-backspace.addEventListener('click', backSpaceIt);
-equalsBtn.addEventListener('click', sumIt);
-operatorBtn.forEach((button) => button.addEventListener('click', () => getOperator(button.textContent)));
-digitBtn.forEach((button) => button.addEventListener('click', () => getNumber(button.textContent)));
-
-// Calculator Functionality
-function clearIt() {
-    console.log("clear button is working");
-    display.textContent = '';
-};
-
-function backSpaceIt() {
-    console.log("backspace button is working");
-    display.textContent = display.textContent.toString().slice(0, -1);
-};
-
-function getNumber(number) {
-    display.textContent += number;
-}
-
-function sumIt(number1, number2) {
-    console.log("equalsBtn clicked");
-} 
-
-function operatorSelector(operator, number1, number2) {
-    number1 = number(number1);
-    number2 = number(number2);
-
-    switch(operator) {
-        case 'x':
-            return multiplyIt(number1, number2);
-        case '/':
-            return divideIt(number1, number2);
-        case '+':
-            return addIt(number1, number2);
-        case '-':
-            return subtractIt(number1, number2);
-        default:
-            return null;
-    };
-};
